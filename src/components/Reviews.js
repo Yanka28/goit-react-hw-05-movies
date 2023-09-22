7c258a31e3454b1e069e0d211db00b48d1fdf924
Yanka28 <yanachepeluk@ukr.net>
2023-09-22 23:49:49 +0300

    vbv

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -1,27 +1,30 @@
-// import { getMoviesReviews } from 'api';
-// import { useEffect, useState } from 'react';
+import { getMoviesReviews } from 'api';
+import { useEffect, useState } from 'react';
+import { useLocation, useParams } from 'react-router-dom';
 
 export const Reviews = () => {
-  // const [reviewsItems, setReviewsItems] = useState([]);
-  // const [loading, setLoading] = useState(false);
-  // const [error, setError] = useState(false);
+  const [reviewsItems, setReviewsItems] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const params = useParams();
+  console.log(params);
 
-  // useEffect(() => {
-  //   async function getReviews() {
-  //     try {
-  //       setLoading(true);
-  //       setError(false);
-  //       const results = await getMoviesReviews();
-  //       setReviewsItems(results);
-  //       console.log(reviewsItems);
-  //     } catch (error) {
-  //       setError(true);
-  //     } finally {
-  //       setLoading(false);
-  //     }
-  //   }
-  //   getReviews();
-  // }, []);
+  useEffect(() => {
+    async function getReviews() {
+      try {
+        setLoading(true);
+        setError(false);
+        const results = await getMoviesReviews(params.movieId);
+        setReviewsItems(results);
+        console.log(reviewsItems);
+      } catch (error) {
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    }
+    getReviews();
+  }, [params.movieId]);
 
-  return;
+  return <div>{/* <p>{reviewsItems}</p> */}</div>;
 };
